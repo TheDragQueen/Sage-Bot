50317f40461f6871557e6b65f88a113db8b470a9
Lian <Lian.Duan@alumni.ubc.ca>
2019-10-06 21:23:53 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,20 @@
 const Discord = require('discord.js');
-const Attachment = require('discord.js');
 const responses = require('./commands/responses.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    client.user.setPresence({ game: { name: 'Lian: Type !gibs', type: 0 } });
+    client.user.setPresence({ game: { name: 'Wrangling', type: 0 } });
 });
 
  
 client.on('message', message => {
-    var response = responses.response(message.content);
     if (message.content === 'dab') {
-        const attachment = new Attachment('https://i.imgur.com/w3duR07.png');
+        const attachment = new Discord.Attachment('https://i.imgur.com/w3duR07.png');
         message.channel.send(`${message.author},`, attachment);
+    }
+    else{
+        var response = responses.response(message.content);
     }
     if (response != null){
         message.channel.send(response);
