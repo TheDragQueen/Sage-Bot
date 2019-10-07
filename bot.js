c2134f9fa604e59a8ef4ea0d9cb9f51f559efdef
Lian <Lian.Duan@alumni.ubc.ca>
2019-10-06 21:46:12 -0700

    funny merme

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    client.user.setPresence({ game: { name: 'Lian: Type !gibs', type: 0 } });
+    client.user.setPresence({ game: { name: 'Vetoing powers', type: 0 } });
 });
 
  
